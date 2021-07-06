19965d28638cfb355401919aea58f9821b963ae8
Jonathan Makunga <jmakunga2015@gmail.com>
2021-07-06 11:33:06 -0400

    Added constants

---
+++ src/com/jm/utils/Constant.java
@@ -0,0 +1,8 @@
+package com.jm.utils;
+
+public class Constant {
+    public static final String SERVER_IP = "192.168.0.8";
+    public static final int SERVER_PORT = 8000;
+    public static final String CLIENT_A_IP = "192.168.0.5";
+    public static final String CLIENT_B_IP = "192.168.0.12";
+}
