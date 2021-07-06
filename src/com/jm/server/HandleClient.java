0f0535099b65d9968de7485c1bef4e404cb2cf5d
Jonathan Makunga <jmakunga2015@gmail.com>
2021-07-06 15:41:20 -0400

    Finish server side

--- src/com/jm/server/HandleClient.java
+++ src/com/jm/server/HandleClient.java
@@ -1,5 +1,6 @@
 package com.jm.server;
 
+import com.jm.utils.Constant;
 import com.jm.utils.SerializedObject;
 import com.jm.utils.Util;
 
@@ -15,10 +16,16 @@ import java.security.NoSuchAlgorithmException;
 public class HandleClient implements Runnable {
     private final Client client;
     private SerializedObject serializedObject;
+    private byte[] random;
+    private final Socket socket;
 
     public HandleClient(Socket socket) throws NoSuchPaddingException, IOException, NoSuchAlgorithmException, InvalidKeyException, ClassNotFoundException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-        byte[] random = Util.generateSecureRandom();
-        client = new Client(socket, Server.keyPair.getPrivate(), random);
+        random = Util.generateSecureRandom();
+        this.socket = socket;
+
+        client = new Client(this.socket, Server.keyPair.getPrivate(), random);
+
+        Server.log.info("Client: " + client.getIp() + ":" + client.getPort());
 
         // Get Client Public Key
         client.updatePublicKey();
@@ -28,17 +35,131 @@ public class HandleClient implements Runnable {
         // Send to client the server's public key
         serializedObject = new SerializedObject();
         serializedObject.setPublicKey(Server.keyPair.getPublic());
-        serializedObject.setFromIP(random);
+        serializedObject.setFromIP(client.encrypt(Constant.SERVER_IP.getBytes()));
+        serializedObject.setToIP(client.encrypt(client.getIp().getBytes()));
+        serializedObject.setOriginIP(client.encrypt(Constant.SERVER_IP.getBytes()));
+
+        // TODO: Encrypt
+        serializedObject.setIvRandom(random);
+
         client.outPutObject(serializedObject);
-        Server.log.info("Sent to client: server's public key and random iv.");
+        Server.log.info("Sent to client: server's public key and random iv(" + Util.byteToHex(random) + ").");
 
         // Add client to connected
         Server.connectedClient.put(client.getIp(), client);
         Server.log.info("client " + client.getIp() + " added to connected list");
+        Server.log.info(Server.connectedClient.toString());
     }
 
     @Override
     public void run() {
+
+        // Client can request another user's public key to kick off chat (subject: initial)
+        // The other user will respond back with an initial message (subject: insta-chat)
+        // if the other user is inactive, the user will respond to the requester (subject: inactive)
+
+        try {
+
+            boolean log_out = false;
+
+            while (!log_out) {
+
+                this.serializedObject = client.inputObject();
+
+                String subject = new String(client.decrypt(serializedObject.getSubject()));
+                String fromIP = new String(client.decrypt(serializedObject.getFromIP()));
+                String toIP = new String(client.decrypt(serializedObject.getToIP()));
+
+                // Log
+                Server.log.info("received a(n) " + subject + " request from " + fromIP + " going to " + toIP);
+
+                if (subject.equalsIgnoreCase(Constant.INITIAL) || subject.equalsIgnoreCase(Constant.INSTA_CHAT)) {
+
+                    if (Server.connectedClient.containsKey(toIP)) {
+                        // Other user is active
+                        Client targetClient = Server.connectedClient.get(toIP);
+
+                        Server.log.info("Targeted user is active");
+
+                        // If subject is insta-chat and the target user is active, transfer the message
+                        if (subject.equalsIgnoreCase(Constant.INSTA_CHAT)) {
+
+                            serializedObject.setFromIP(targetClient.encrypt(Constant.SERVER_IP.getBytes()));
+                            serializedObject.setOriginIP(targetClient.encrypt(fromIP.getBytes()));
+                            serializedObject.setToIP(targetClient.encrypt(toIP.getBytes()));
+
+                            // TODO: check that the message body is not null
+
+                            targetClient.outPutObject(serializedObject);
+
+                            Server.log.info("Message transferred to " + targetClient.getIp());
+                        }
+
+                        // If subject is initial and the target user is active:
+                        //      Send:
+                        //              subject: Active
+                        //              to requester: receiver's public key
+                        //              to receiver: requesters public key
+                        //              to both: an unique secure random
+                        else {
+
+                            random = Util.generateSecureRandom();
+
+                            // To the requester
+                            serializedObject.setSubject(client.encrypt(Constant.ACTIVE.getBytes()));
+                            serializedObject.setFromIP(client.encrypt(Constant.SERVER_IP.getBytes()));
+                            serializedObject.setToIP(client.encrypt(fromIP.getBytes()));
+                            serializedObject.setOriginIP(client.encrypt(Constant.SERVER_IP.getBytes()));
+                            serializedObject.setPublicKey(targetClient.getOtherPublicKey());
+
+                            // TODO: Encrypt random before sending it over the network
+                            serializedObject.setIvRandom(random);
+
+                            client.outPutObject(serializedObject);
+                            Server.log.info("Sent the target user ( " + targetClient.getIp() + " ) Public key to " + client.getIp());
+
+
+                            // To the receiver
+                            serializedObject.setSubject(targetClient.encrypt(Constant.ACTIVE.getBytes()));
+                            serializedObject.setFromIP(targetClient.encrypt(Constant.SERVER_IP.getBytes()));
+                            serializedObject.setToIP(targetClient.encrypt(toIP.getBytes()));
+                            serializedObject.setOriginIP(targetClient.encrypt(fromIP.getBytes()));
+                            serializedObject.setPublicKey(client.getOtherPublicKey());
+
+                            targetClient.outPutObject(serializedObject);
+                            Server.log.info("Sent the current user's ( " + client.getIp() + " ) Public key to " + targetClient.getIp());
+                        }
+                        // If subject is log out: remove the client in the connected list, terminate both the socket and thread
+                    } else {
+                        // Other user is inactive
+                        Server.log.info("Targeted user is inactive");
+
+                        // Otherwise, send back to the requester an inactive subject
+                        serializedObject.setSubject(client.encrypt(Constant.INACTIVE.getBytes()));
+                        serializedObject.setFromIP(client.encrypt(Constant.SERVER_IP.getBytes()));
+                        serializedObject.setToIP(client.encrypt(fromIP.getBytes()));
+                        serializedObject.setOriginIP(client.encrypt(Constant.SERVER_IP.getBytes()));
+
+                        client.outPutObject(serializedObject);
+                        Server.log.info("Sent inactive subject to " + client.getIp());
+                    }
+
+                }
+                else if (subject.equalsIgnoreCase(Constant.LOGOUT)) {
+                    log_out = true;
+                }
+                // Unsupported subject
+            }
+
+            // Close thread/Socket
+            Server.log.info("terminating thread " + Thread.currentThread().getName()
+                    + "\nTerminating the current socket " + this.socket.getInetAddress().getHostAddress());
+            this.socket.close();
+            Thread.currentThread().interrupt();
+
+        } catch (IOException | ClassNotFoundException | InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/com/jm/utils/Constant.java
+++ src/com/jm/utils/Constant.java
@@ -7,4 +7,9 @@ public class Constant {
     public static final String CLIENT_B_IP = "192.168.0.12";
     public static final String KEY_ALGO = "DH";
     public static final int KEY_SIZE = 1024;
+    public static final String INITIAL = "Initial";
+    public static final String ACTIVE = "Active";
+    public static final String INACTIVE = "Inactive";
+    public static final String INSTA_CHAT = "Insta-chat";
+    public static final String LOGOUT = "Log out";
 }
