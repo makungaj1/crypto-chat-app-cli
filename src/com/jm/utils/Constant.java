6e6050a29668abfcaa9768e9dde1500504dc9530
Jonathan Makunga <jmakunga2015@gmail.com>
2021-07-06 20:53:00 -0400

    Change ips

--- src/com/jm/utils/Constant.java
+++ src/com/jm/utils/Constant.java
@@ -3,8 +3,8 @@ package com.jm.utils;
 public class Constant {
     public static final String SERVER_IP = "192.168.0.8";
     public static final int SERVER_PORT = 8000;
-    public static final String CLIENT_A_IP = "192.168.0.5";
-    public static final String CLIENT_B_IP = "192.168.0.12";
+    public static final String CLIENT_A_IP = "192.168.0.12";
+    public static final String CLIENT_B_IP = "192.168.0.5";
     public static final String KEY_ALGO = "DH";
     public static final int KEY_SIZE = 1024;
     public static final String INITIAL = "Initial";
